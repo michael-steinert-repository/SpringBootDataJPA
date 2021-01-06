bc76d34c21aaeb18fd2d429b06267c46c7274747
Michael Steinert <michael.steinert.94@gmail.com>
2021-01-06 10:46:38 +0100

    Added Transactions to Student-Repository

--- src/main/java/com/example/SpringBootDataJPA/StudentRepository.java
+++ src/main/java/com/example/SpringBootDataJPA/StudentRepository.java
@@ -4,11 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional(readOnly = true)
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
@@ -19,8 +22,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query(value = "SELECT * FROM Student WHERE first_name = :firstName AND age >= :age", nativeQuery = true)
     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNative(@Param("firstName")String firstName, @Param("age")Integer age);
 
-    @Transactional
     @Modifying
+    @Transactional(readOnly = false)
     @Query("DELETE FROM Student s WHERE s.email = ?1")
     int deleteStudentByEmail(String email);
 }
